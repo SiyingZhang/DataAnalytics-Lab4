1ba55a0b0a8106b98c11b3c1bb8514e3decdb817
SiyingZhang <siyingzhang@icloud.com>
2015-10-10 16:39:53 -0400

    updated.

--- Lab4-Siying.R
+++ Lab4-Siying.R
@@ -4,13 +4,16 @@ library(ggplot2)
 train <- read.csv("/Users/siying/Documents/edu/PITT/2015\ Fall/Data\ Analytics/Lab/DataAnalytics-Lab4/train.csv")
 
 ## Select survived people
-train0 <- train[which(train$Survived == 1),]
+trainSurvived <- train[which(train$Survived == 1),]
 ## To see the summary of age value
-summary(train0$Age) # Min 0.42  Median 28.00  Mean28.34  Max 80.00
+summary(trainSurvived$Age) # Min 0.42  Median 28.00  Mean28.34  Max 80.00
 
+attach(trainSurvived)
 ## Use geom_histogram to present the count of the age of survived people 
-(p <- ggplot(data = train0, aes(train0$Age)) + geom_histogram(breaks = seq(0, 80, by = 5), col = "white", aes(fill = ..count..), alpha = 0.7) +
+(p <- ggplot(data = trainSurvived, aes(trainSurvived$Age)) + geom_histogram(breaks = seq(0, 80, by = 5), col = "white", aes(fill = ..count..), alpha = 0.7) +
 	scale_fill_gradient("Count", low = "pink", high = "purple") +
 	labs(title = "Age of survived people") + 
-	labs(x = "Age", y = "Count")
+	labs(x = "Age", y = "Count") +
+	ylim(c(0, 50))
 )
+
